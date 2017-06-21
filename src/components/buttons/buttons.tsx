48779869f3bbeabddf3a98218b3c5964e24c0aa9
Brandy Carney <brandyscarney@gmail.com>
2017-06-21 13:26:11 -0400

    chore(ionic): update from core

--- src/components/buttons/buttons.tsx
+++ src/components/buttons/buttons.tsx
@@ -10,8 +10,8 @@ import { Component, h } from '../index';
 export class Buttons {
   $el: HTMLElement;
 
-  ionViewWillLoad() {
     // Add bar-button classes to each ion-button
+  ionViewDidLoad() {
     const buttons = this.$el.querySelectorAll('ion-button') as any;
     for (var i = 0; i < buttons.length; i++) {
       buttons[i].setAttribute('button-type', 'bar-button');
